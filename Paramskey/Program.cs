ff2c55a3fe8ae86d949dfb37941e188bc0dc1841
rupesh palve <rupesh.palve48@outlook.com>
2022-09-19 18:13:39 -0400

    Add project files.

---
+++ Paramskey/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paramskey
+{
+    internal class Program
+    {
+        static void AcceptNames(params string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("-------------");
+        }
+
+        static void Main(string[] args)
+        {
+            AcceptNames("dipu", "rani", "madhu");
+            AcceptNames("Rupu", "jay");
+            AcceptNames("darshan", "rutu", "ravi", "rechal");
+        }
+    }
+}
